136d3fe97d8bac3b79f4cac4467c56b3bb1ab9e7
Islombek Ergashev <islombekergashev@Islombeks-MacBook-Pro.local>
2022-08-02 23:20:09 +0900

    changing navbar darkmode color

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div
       className="n-wrapper"
-      style={{ backgroundColor: darkMode ? "" : "#fff" }}
+      style={{ backgroundColor: darkMode ? "#000" : "#fff" }}
     >
       <div className="n-left">
         <div className="n-name">Islombek</div>
